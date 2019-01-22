30d0db7dc452059c78546c2b7d7a67cc505c7280
Andrew Otte <ottenc98@gmail.com>
2019-01-22 16:22:38 -0500

    testing python with numbers

---
+++ pythontest.py
@@ -0,0 +1,29 @@
+print("hello")
+
+print(3+3)
+
+print(3*3)
+
+print(19/3)
+
+print(19//3)
+
+print(19%3)
+
+print(18/3)
+
+print(5**2)
+
+a = 10
+b = 20
+print(a+b)
+# <-- Comment
+# print(c+1)
+
+def addandmultiply(a,b):
+	print(a+b)
+	print(a*b)
+
+addandmultiply(10,20)
+
+print(round(3.14159,3))
