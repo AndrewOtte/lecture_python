1794cd53a2b56c6eadb86c46255b31f75ced6f1b
Andrew Otte <ottenc98@gmail.com>
2019-01-22 16:40:06 -0500

    added elif, y, and animals

--- conditional.py
+++ conditional.py
@@ -1,6 +1,29 @@
-x = 10
+x = 5
 
 if x < 0: 
 	print("X is smaller than 0.")
+elif x > 10:
+	print("X is bigger than 10.")
 else:
-	print("X is not smaller than 0.")
+	print("X is between 0 and 10.")
+
+###########
+#y= int(input("Enter value of y:"))
+
+y=10
+if y < 0:
+	print("y is small.")
+elif y >=0: 
+	print("y is big.")
+else:
+	print("error")
+
+############
+
+animals = ['cat','dog','turtle','bird']
+
+print(animals)
+
+for x in animals:
+	print(x)
+	print(len(x))
